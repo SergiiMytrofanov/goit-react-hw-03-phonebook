650d11b906e605ac76573e6d0fb300dad33f5782
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-07-31 23:31:56 +0300

    local storage contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,25 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './FilterItem/FilterItem';
 import styles from './App.module.css'
+import { saveContactsToLocalStorage, loadContactsFromLocalStorage } from './ContactLocalStorage/localStoraje';
 
 class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '+380-459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '+380-443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '+380 645 17 79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '+380 227 91 26'},
-    ],
+    contacts: [],
     filter: ''
   };
+
+componentDidMount() {
+  const storedContacts = loadContactsFromLocalStorage();
+  this.setState({contacts:storedContacts});
+}
+
+
+componentDidUpdate(prevProps, prevState) {
+  if(prevState.contacts != this.state.contacts) {
+    saveContactsToLocalStorage(this.state.contacts);
+  }
+}
 
   addContact = (newContact) => {
     const { contacts } = this.state;
